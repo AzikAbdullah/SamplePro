94898bc6e77082120033f35932bdbba8dce53af9
N, Azik Abdullah (Cognizant) <AzikAbdullah.N@cognizant.com>
2020-06-26 23:45:53 +0530

    no message

---
+++ SamplePro/SamplePro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SamplePro
+//
+//  Created by N, Azik Abdullah (Cognizant) on 26/06/20.
+//  Copyright © 2020 N, Azik Abdullah (Cognizant). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
